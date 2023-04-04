8ecbed34267d56110872a5ddc9dcf65c7f71d78d
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-04-04 15:22:06 +0200

    Create Worker

---
+++ src/main/resources/theme-resources/resources/pi-webWorkerUtils.js
@@ -0,0 +1,38 @@
+let w;
+
+function startPollWorker()
+{
+    if (typeof (Worker) !== "undefined")
+    {
+        if (typeof (w) == "undefined")
+        {
+            w = new Worker("pi-pollTransaction.js");
+            console.log("Setting new Worker..."); //todo rm
+        }
+        w.onmessage = function (event)
+        {
+            if (event.data === true)
+            {
+                console.log("privacyIDEA: Poll transaction result succeeded!");
+                document.forms["kc-otp-login-form"].submit();
+            }
+            else
+            {
+                console.log("privacyIDEA:" + event.data);
+                console.log("privacyIDEA: Poll transaction in browser failed. Please contact the administrator.");
+                sessionStorage.removeItem("piServerURL");
+                sessionStorage.removeItem("piTransactionID");
+
+                // Fallback to standard poll transaction
+                document.forms["kc-otp-login-form"].submit();
+            }
+        };
+    }
+}
+
+function stopPollWorker()
+{
+    sessionStorage.setItem("pollInBrowserFailed", "true");
+    w.terminate();
+    w = undefined;
+}
