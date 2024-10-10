c6b2c49e01965687fc1955613f798dd9450e0f03
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2024-10-10 14:57:48 +0200

    Create pi-utils.js

---
+++ src/main/resources/theme-resources/resources/pi-utils.js
@@ -0,0 +1,59 @@
+window.piGetValue = function getValue(id)
+{
+    const element = document.getElementById(id);
+    if (element === null)
+    {
+        console.log(id + " is null!");
+        return "";
+    }
+    else
+    {
+        return element.value;
+    }
+}
+
+window.piSetValue = function setValue(id, value)
+{
+    const element = document.getElementById(id);
+    if (element !== null)
+    {
+        element.value = value;
+    }
+    else
+    {
+        console.log(id + " is null!");
+    }
+}
+
+window.piDisableElement = function disableElement(id)
+{
+    const element = document.getElementById(id);
+    if (element !== null)
+    {
+        element.style.display = "none";
+    }
+    else
+    {
+        console.log(id + " is null!");
+    }
+}
+
+window.piEnableElement = function enableElement(id)
+{
+    const element = document.getElementById(id);
+    if (element !== null)
+    {
+        element.style.display = "initial";
+    }
+    else
+    {
+        console.log(id + " is null!");
+    }
+}
+
+window.piChangeMode = function changeMode(newMode)
+{
+    document.getElementById("mode").value = newMode;
+    document.getElementById("modeChanged").value = "1";
+    document.forms["piLoginForm"].submit();
+}
